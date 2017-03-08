8d9a251a8a9607f7773236a21740961585d0ade7
Sylvester C. Reventhal Abelsen <cornelius.a.r@gmail.com>
2017-03-08 12:24:19 +0100

    implemented mask of varying color

---
+++ generateMask.m
@@ -0,0 +1,129 @@
+%maskFontSize = 70;
+%letterBoxX = 46;
+%letterBoxY = 53;
+
+%A_Green = makeLetterImg(maskFontSize, 'A', 'green', letterBoxX, letterBoxY);
+%letters = ['-' '|' '^' '<' '\' '*' '~' '&' '=' '{' '[' ']' '}'];
+%perm = [2 1 4 3];
+%colors = cellstr(['green'; 'red  '; 'red  '; 'green']);
+%makeLetterCombination(letters, perm, colors, maskFontSize, letterBoxX, letterBoxY);
+%colors = cellstr(['green'; 'red  ']);
+%mask = getMask(letters, colors, maskFontSize, letterBoxX, letterBoxY);
+%figure
+%imshow(mask)
+
+function mask = getMask(letters, colors, fontSize, letterBoxX, letterBoxY)
+mask = ones(letterBoxY*3, letterBoxX*3, 3);
+
+for x = 1:3
+    for y = 1:3
+        colorSequence = randperm(2);
+        letterColors = [colors(colorSequence(1)) colors(colorSequence(2)) colors(colorSequence(2)) colors(colorSequence(1))];
+        letterSequence = randperm(size(letters, 2));
+        letter = makeLetterCombination(letters, letterSequence, letterColors, fontSize, letterBoxX, letterBoxY);
+        m_x = (x-1) * letterBoxX;
+        m_y = (y-1) * letterBoxY;
+        mask(1+m_y:letterBoxY+m_y, 1+m_x:letterBoxX+m_x, :) = letter(:, :, :);    
+    end
+end
+return
+end
+
+function letterCombination = makeLetterCombination(letters, perm, colors, fontSize, letterBoxX, letterBoxY)
+letterCombination = ones(letterBoxY, letterBoxX, 3);
+NW = makeLetterImg(fontSize, letters(perm(1)), colors(1), letterBoxX, letterBoxY);
+NE = makeLetterImg(fontSize, letters(perm(2)), colors(2), letterBoxX, letterBoxY);
+SW = makeLetterImg(fontSize, letters(perm(3)), colors(3), letterBoxX, letterBoxY);
+SE = makeLetterImg(fontSize, letters(perm(4)), colors(4), letterBoxX, letterBoxY);
+
+borderX = floor(letterBoxX/2);
+borderY = floor(letterBoxY/2);
+letterCombination(1:borderY, 1:borderX, :) = NW(1:borderY, 1:borderX, :);
+letterCombination(1:borderY, borderX+1:letterBoxX, :) = NE(1:borderY, borderX + 1:letterBoxX, :);
+letterCombination(borderY+1:letterBoxY, 1:borderX, :) = SW(borderY+1:letterBoxY, 1:borderX, :);
+letterCombination(borderY+1:letterBoxY, borderX + 1:letterBoxX, :) = SE(borderY+1:letterBoxY, borderX + 1:letterBoxX, :);
+return
+end
+
+function letterImg = makeLetterImg(fontSize, letter, letterColor, letterBoxX, letterBoxY)
+maskImage = ones(fontSize,fontSize,3);
+maskX = fontSize*1.5;
+maskY = fontSize*1.5;
+for y = 1:maskY
+    for x = 1:maskX
+        for c = 1:3
+            maskImage(y, x, c) = 0.5;
+        end
+    end
+end
+
+position = [0 0];
+box_color = {'black'};
+RGB = insertText(maskImage,position,letter,'FontSize',fontSize,'BoxColor',...
+    box_color,'BoxOpacity',0.0,'TextColor',letterColor);
+
+m_top = 0;
+for y = 1:maskY
+    if m_top ~= 0; break; end
+    for x = 1:maskX
+        for c = 1:3
+            r = RGB(y, x, 1);
+            g = RGB(y, x, 2);
+            b = RGB(y, x, 3);
+            if r ~= g | r ~= b; m_top = y; break; end
+        end
+    end
+end
+
+m_bottom = 0;
+for y = 1:maskY
+    if m_bottom ~= 0; break; end
+    for x = 1:maskX
+        for c = 1:3
+            r = RGB(maskY - (y-1), x, 1);
+            g = RGB(maskY - (y-1), x, 2);
+            b = RGB(maskY - (y-1), x, 3);
+            if r ~= g | r ~= b; m_bottom = maskY - (y-1); break; end
+        end
+    end
+end
+
+m_left = 0;
+for x = 1:maskX
+    if m_left ~= 0; break; end
+    for y = 1:maskY
+        for c = 1:3
+            r = RGB(y, x, 1);
+            g = RGB(y, x, 2);
+            b = RGB(y, x, 3);
+            if r ~= g | r ~= b; m_left = x; break; end
+        end
+    end
+end
+
+m_right = 0;
+for x = 1:maskX
+    if m_right ~= 0; break; end
+    for y = 1:maskY
+        for c = 1:3
+            r = RGB(y, maskX - x + 1, 1);
+            g = RGB(y, maskX - x + 1, 2);
+            b = RGB(y, maskX - x + 1, 3);
+            if r ~= g | r ~= b; m_right = maskX - x + 1; break; end
+        end
+    end
+end
+
+while(m_right - m_left + 1 < letterBoxX)
+    if(m_left > 1); m_left = m_left - 1; end
+    if(m_right - m_left + 1  < letterBoxX); m_right = m_right + 1; end
+end
+
+while(m_bottom - m_top + 1 < letterBoxY)
+    if(m_top > 1); m_top = m_top - 1; end
+    if(m_bottom - m_top + 1 < letterBoxY); m_bottom = m_bottom + 1; end
+end
+letterImg = RGB(m_top:m_bottom, m_left:m_right, 1:3);
+
+return
+end
--- visualtheorytest.m
+++ visualtheorytest.m
@@ -4,25 +4,42 @@ close all;
 clearvars;
 sca;
 
-exptrials = 5;           %number of trials per exposure duration
-primedur = 1.0;          %time to show the priming symbol
-primetotrialdelay = 2.0; %delay between prime symbol dissapearance and trial
-primechar = '*';
-mask = '-|^<\*';
-maskdur = 2.0;           %duration the mask is shown
-stimulussize = 70;       %text size for the stimulus letters
+screenDiag = 31.75; %cm
+screenPixelsX = 1366.0;
+screenPixelsY = 768.0;
+screenPixelsDiag = sqrt(screenPixelsX.^2 + screenPixelsY.^2);
+cmPerPixel = screenDiag/screenPixelsDiag;
 
-stopprogram = 0;
-enterkey = 10;
-escapekey = KbName('ESCAPE');
+disp(screenPixelsDiag);
+disp(cmPerPixel);
+screenCmX = screenPixelsX * cmPerPixel;
+screenCmY = screenPixelsY * cmPerPixel;
+disp(screenCmX);
+disp(screenCmY);
+letterDisplacement = 5.0; %cm
+
+
+expTrials = 5;           %number of trials per exposure duration
+primeDur = 1.0;          %time to show the priming symbol
+primeToTrialDelay = 2.0; %delay between prime symbol dissapearance and trial
+primeChar = '+';
+mask = '-|^<\*';
+maskDur = 2.0;           %duration the mask is shown
+stimulusSize = 70;       %text size for the stimulus letters
+stimulusPositionCorrection = 0.47 / cmPerPixel;
+
 data_responses = 1;
 data_duration = 2;
 data_time = 3;
 
+enterKey = 10;
+escapeKey = KbName('ESCAPE');
+stopProgram = 0;
+
 
 letters = ['A' 'E' 'I' 'O' 'U'];
-numletters = size(letters, 2);
-expdurations = [0.5 0.15];
+numLetters = size(letters, 2);
+expDurations = [0.5 0.15];
 typeRedLetter = 'Please press symbol of the RED letter';
 typeGreenLetter = 'Please press the symbol of the GREEN letter';
 
@@ -38,302 +55,65 @@ red = [1 0 0];
 green = [0 1 0];
 grey = white / 2;
 
-letterspacing = 0.20;
-
 [window, windowRect] = PsychImaging('OpenWindow', screenNumber, grey);
 [screenXpixels, screenYpixels] = Screen('WindowSize', window);
 [xCenter, yCenter] = RectCenter(windowRect);
-left = xCenter * (1.0 - letterspacing);
-right = xCenter * (1.0 + letterspacing);
-startingpositions = [left right];
+left = xCenter - letterDisplacement/cmPerPixel - stimulusPositionCorrection;
+right = xCenter + letterDisplacement/cmPerPixel - stimulusPositionCorrection;
+startingPositions = [left right];
 leftToRight = right - left;
 
-data_distinct_single_red = zeros(3, size(expdurations, 2), exptrials);
+data_distinct_single_red = zeros(3, size(expDurations, 2), expTrials);
 
+%MASK
+maskSymbols = ['-' '|' '^' '<' '\' '*' '~' '&' '=' '{' '[' ']' '}'];
+maskColors = cellstr(['green'; 'red  ']);
+maskFontSize = 70;
+letterBoxX = 46;
+letterBoxY = 53;
+
+%KEYBOARD Queue
 ListenChar(-1);
 KbQueueCreate();
 
-%{
-%distinct SINGLE red **********************************
-vbl = showTrialInformation('Distinct - single (RED)', window, yCenter*1.4, white, enterkey);
-letterSequence = generateLetterSequence(letters, size(expdurations, 2), exptrials);
-for e = 1:size(expdurations, 2)
-    if(stopprogram); break; end
-    for t = 1:exptrials
-        if(stopprogram); break; end
-        expduration = expdurations(e);
-        vbl = showPrimeChar(window, primechar, primedur, white);
-    
-        screenpos = left + (randi(2) - 1) * leftToRight;
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, letterSequence(e, t), screenpos, 'center', red);
-        vbl = Screen('Flip', window, vbl + primetotrialdelay); %SHOW STIMULUS
-        time_start = GetSecs();
-        KbQueueStart();
-        
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, mask, screenpos, 'center', white);    
-        vbl = Screen('Flip', window, vbl + expduration); %#ok<NASGU> %SHOW MASK  
-        
-        firstPress = getResponseOneAnswer(time_start, window, maskdur + expduration, typeRedLetter, white);      
-        KbQueueStop();
-        responsetime = firstPress(find(firstPress)) - time_start;
-        if(min(find(firstPress)) == escapekey); stopprogram = 1; end
-    end
-end
-%}
 
-%distinct PARTIAL red **********************************
-vbl = showTrialInformation('Distinct - partial (RED)', window, yCenter*1.4, white, enterkey);
-letterSequenceRed = generateLetterSequence(letters, size(expdurations, 2), exptrials);
-letterSequenceGreen = generateLetterSequence(letters, size(expdurations, 2), exptrials);
-for e = 1:size(expdurations, 2)
-    if(stopprogram); break; end
-    for t = 1:exptrials
-        if(stopprogram); break; end
-        expduration = expdurations(e);   
-        vbl = showPrimeChar(window, primechar, primedur, white);
-        positions = startingpositions(randperm(2));
-    
-        Screen('TextSize', window, stimulussize);
+vbl = showTrialInformation('Distinct - partial (RED)', window, yCenter*1.4, white, enterKey);
+letterSequenceRed = generateLetterSequence(letters, size(expDurations, 2), expTrials);
+letterSequenceGreen = generateLetterSequence(letters, size(expDurations, 2), expTrials);
+for e = 1:size(expDurations, 2)
+    if(stopProgram); break; end
+    for t = 1:expTrials
+        if(stopProgram); break; end
+        maskImg = generateMask(maskSymbols, maskColors, maskFontSize, letterBoxX, letterBoxY);
+        textureIndex=Screen('MakeTexture', window, maskImg);
+        expduration = expDurations(e);   
+        vbl = showPrimeChar(window, primeChar, primeDur, white);
+        positions = startingPositions(randperm(2));
+  
+        Screen('TextSize', window, stimulusSize);
         DrawFormattedText(window, letterSequenceRed(e, t), positions(1), 'center', red);
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, letterSequenceGreen(e, t), positions(2), 'center', green);       
-        vbl = Screen('Flip', window, vbl + primetotrialdelay); %SHOW STIMULUS
+        DrawFormattedText(window, letterSequenceGreen(e, t), positions(2), 'center', green);   
+
+        vbl = Screen('Flip', window, vbl + primeToTrialDelay); %SHOW STIMULUS
         time_start = GetSecs();
         KbQueueStart();
         
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, mask, positions(1), 'center', white);
-        DrawFormattedText(window, mask, positions(2), 'center', white);
-        vbl = Screen('Flip', window, vbl + expdurations(e)); %SHOW MASK
+        dstRects = [left - letterBoxX*1.5, yCenter - letterBoxY*1.5, left + letterBoxX*1.5, yCenter + letterBoxY*1.5];
+        Screen('DrawTextures', window, textureIndex, [], dstRects, [], [], [], []);
+        dstRects = [right - letterBoxX*1.5, yCenter - letterBoxY*1.5, right + letterBoxX*1.5, yCenter + letterBoxY*1.5];
+        Screen('DrawTextures', window, textureIndex, [], dstRects, [], [], [], []);
+        vbl = Screen('Flip', window, vbl + expDurations(e)); %SHOW MASK
         
-        firstPress = getResponseOneAnswer(time_start, window, maskdur + expduration, typeRedLetter, white);      
+        
+        firstPress = getResponseOneAnswer(time_start, window, maskDur + expduration, 150, red);      
         KbQueueStop();
         responsetime = firstPress(find(firstPress)) - time_start;
-        if(min(find(firstPress)) == escapekey); stopprogram = 1; end
+        if(min(find(firstPress)) == escapeKey); stopProgram = 1; end
     end
 end
 
 ListenChar(0);
 sca;
-
-%{
-
-%distinct SINGLE green **********************************
-Screen('TextSize', window, 100);
-DrawFormattedText(window, 'Distinct - single (green)', 'center', 'center', white);
-Screen('TextSize', window, 90);
-DrawFormattedText(window, 'Press a key to continue', 'center', yCenter*1.4, white);
-vbl = Screen('Flip', window);
-[ch, when] = GetChar();
-KbStrokeWait;
-
-for e = 1:size(expdurations, 2)
-    for t = 1:exptrials
-        Screen('TextSize', window, 200);
-        DrawFormattedText(window, primechar, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        vbl = Screen('Flip', window, vbl + primedur);
-    
-        letter = letters(randi(numletters));
-        screenpos = left + (randi(2) - 1) * leftToRight;
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, letter, screenpos, 'center', green);
-
-        vbl = Screen('Flip', window, vbl + primetotrialdelay);
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, mask, screenpos, 'center', white);
-        vbl = Screen('Flip', window, vbl + expdurations(e));
-        vbl = Screen('Flip', window, vbl + maskdur);
-        
-        Screen('TextSize', window, 50);
-        DrawFormattedText(window, typeGreenLetter, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        [ch, when] = GetChar();
-        KbStrokeWait;
-    end
-end
-
-%distinct PARTIAL red **********************************
-Screen('TextSize', window, 100);
-DrawFormattedText(window, 'Distinct - partial (red)', 'center', 'center', white);
-Screen('TextSize', window, 90);
-DrawFormattedText(window, 'Press a key to continue', 'center', yCenter*1.4, white);
-vbl = Screen('Flip', window);
-[ch, when] = GetChar();
-KbStrokeWait;
-
-for e = 1:size(expdurations, 2)
-    for t = 1:exptrials
-        positions = startingpositions(randperm(2));
-        randletters = letters(randperm(numletters));
-        
-        Screen('TextSize', window, 200);
-        DrawFormattedText(window, primechar, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        vbl = Screen('Flip', window, vbl + primedur);
-    
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, randletters(1), positions(1), 'center', red);
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, randletters(2), positions(2), 'center', green);
-        
-        vbl = Screen('Flip', window, vbl + primetotrialdelay);
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, mask, positions(1), 'center', white);
-        DrawFormattedText(window, mask, positions(2), 'center', white);
-        vbl = Screen('Flip', window, vbl + expdurations(e));
-        vbl = Screen('Flip', window, vbl + maskdur);
-        
-        Screen('TextSize', window, 50);
-        DrawFormattedText(window, typeRedLetter, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        [ch, when] = GetChar();
-        KbStrokeWait;
-    end
-end
-
-%distinct PARTIAL green **********************************
-Screen('TextSize', window, 100);
-DrawFormattedText(window, 'Distinct - partial (green)', 'center', 'center', white);
-Screen('TextSize', window, 90);
-DrawFormattedText(window, 'Press a key to continue', 'center', yCenter*1.4, white);
-vbl = Screen('Flip', window);
-[ch, when] = GetChar();
-KbStrokeWait;
-
-for e = 1:size(expdurations, 2)
-    for t = 1:exptrials
-        positions = startingpositions(randperm(2));
-        randletters = letters(randperm(numletters));
-        
-        Screen('TextSize', window, 200);
-        DrawFormattedText(window, primechar, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        vbl = Screen('Flip', window, vbl + primedur);
-    
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, randletters(1), positions(1), 'center', red);
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, randletters(2), positions(2), 'center', green);
-        
-        vbl = Screen('Flip', window, vbl + primetotrialdelay);
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, mask, positions(1), 'center', white);
-        DrawFormattedText(window, mask, positions(2), 'center', white);
-        vbl = Screen('Flip', window, vbl + expdurations(e));
-        vbl = Screen('Flip', window, vbl + maskdur);
-        
-        Screen('TextSize', window, 50);
-        DrawFormattedText(window, typeGreenLetter, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        [ch, when] = GetChar();
-        KbStrokeWait;
-    end
-end
-
-%distinct WHOLE red **********************************
-Screen('TextSize', window, 100);
-DrawFormattedText(window, 'Distinct - whole (red)', 'center', 'center', white);
-Screen('TextSize', window, 90);
-DrawFormattedText(window, 'Press a key to continue', 'center', yCenter*1.4, white);
-vbl = Screen('Flip', window);
-[ch, when] = GetChar();
-KbStrokeWait;
-
-for e = 1:size(expdurations, 2)
-    for t = 1:exptrials
-        positions = startingpositions(randperm(2));
-        randletters = letters(randperm(numletters));
-        
-        Screen('TextSize', window, 200);
-        DrawFormattedText(window, primechar, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        vbl = Screen('Flip', window, vbl + primedur);
-    
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, randletters(1), positions(1), 'center', red);
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, randletters(2), positions(2), 'center', green);
-        
-        vbl = Screen('Flip', window, vbl + primetotrialdelay);
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, mask, positions(1), 'center', white);
-        DrawFormattedText(window, mask, positions(2), 'center', white);
-        vbl = Screen('Flip', window, vbl + expdurations(e));
-        vbl = Screen('Flip', window, vbl + maskdur);
-        
-        Screen('TextSize', window, 50);
-        DrawFormattedText(window, typeRedLetter, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        [ch_red, when_red] = GetChar();
-        KbStrokeWait;
-        Screen('TextSize', window, 50);
-        DrawFormattedText(window, typeGreenLetter, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        [ch_green, when_green] = GetChar();
-        KbStrokeWait;
-    end
-end
-
-%distinct WHOLE green **********************************
-Screen('TextSize', window, 100);
-DrawFormattedText(window, 'Distinct - whole (green)', 'center', 'center', white);
-Screen('TextSize', window, 90);
-DrawFormattedText(window, 'Press a key to continue', 'center', yCenter*1.4, white);
-vbl = Screen('Flip', window);
-[ch, when] = GetChar();
-KbStrokeWait;
-
-for e = 1:size(expdurations, 2)
-    for t = 1:exptrials
-        positions = startingpositions(randperm(2));
-        randletters = letters(randperm(numletters));
-        
-        Screen('TextSize', window, 200);
-        DrawFormattedText(window, primechar, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        vbl = Screen('Flip', window, vbl + primedur);
-    
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, randletters(1), positions(1), 'center', red);
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, randletters(2), positions(2), 'center', green);
-        
-        vbl = Screen('Flip', window, vbl + primetotrialdelay);
-        Screen('TextSize', window, stimulussize);
-        DrawFormattedText(window, mask, positions(1), 'center', white);
-        DrawFormattedText(window, mask, positions(2), 'center', white);
-        vbl = Screen('Flip', window, vbl + expdurations(e));
-        vbl = Screen('Flip', window, vbl + maskdur);
-        
-        Screen('TextSize', window, 50);
-        DrawFormattedText(window, typeGreenLetter, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        [ch_red, when_red] = GetChar();
-        KbStrokeWait;
-        Screen('TextSize', window, 50);
-        DrawFormattedText(window, typeRedLetter, 'center', 'center', white);
-        vbl = Screen('Flip', window);
-        [ch_green, when_green] = GetChar();
-        KbStrokeWait;
-    end
-end
-
-%END OF TEST **********************************
-Screen('TextSize', window, 100);
-DrawFormattedText(window, 'End of test, thank you!', 'center', 'center', white);
-Screen('TextSize', window, 90);
-DrawFormattedText(window, 'Press a key to continue', 'center', yCenter*1.4, white);
-vbl = Screen('Flip', window);
-[ch, when] = GetChar();
-KbStrokeWait;
-
-% Clear the screen
-sca;
- %}
-
 
 function letterSequence = generateLetterSequence(letters, expdurations, exptrials)
 letterSequence = zeros(expdurations, exptrials);
@@ -361,7 +141,7 @@ end
 return
 end
 
-function firstPress = getResponseOneAnswer(time_start, window, waitduration, stimulusMessage, messageColor)
+function firstPress = getResponseOneAnswer(time_start, window, waitduration, messageSize, messageColor)
 time_check = 0;
 pressed = 0;
 while(time_check - time_start < waitduration & ~pressed)
@@ -371,8 +151,8 @@ end
 vbl = Screen('Flip', window);
 
 if ~pressed
-    Screen('TextSize', window, 50);
-    DrawFormattedText(window, stimulusMessage, 'center', 'center', messageColor);
+    Screen('TextSize', window, messageSize);
+    DrawFormattedText(window, '?', 'center', 'center', messageColor);
     vbl = Screen('Flip', window);
 end
         
